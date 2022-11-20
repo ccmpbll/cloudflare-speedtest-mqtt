43ae97a28416b0959a90ce5574fd6eb24d9b409b
Chris Campbell <ccmpbll@users.noreply.github.com>
2022-11-20 16:08:19 -0500

    Create speedtest.sh

---
+++ speedtest.sh
@@ -0,0 +1,22 @@
+!/bin/bash
+while true
+do
+	echo "Running CloudFlare Speedtest..."
+  
+  touch /tmp/speedtest-result
+	cfspeedtest --json > /tmp/speedtest-result
+  RESULT=$(cat /tmp/speedtest-result)
+
+	if [[ "${MQTT_PASS}" ]]; then
+	echo "Sending Data to MQTT ($MQTT_SERVER)..."
+		mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC -m $RESULT
+	else
+	echo "Sending Data to MQTT ($MQTT_SERVER) -  No Auth"
+		mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC -m $RESULT
+	fi 
+
+	echo "Sleeping for $SLEEP Seconds..."
+	
+	sleep $SLEEP 
+
+done
