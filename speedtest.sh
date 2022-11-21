7f1a9c9ef6997b9b01f4b7a23797732866b587ae
Chris Campbell <ccmpbll@users.noreply.github.com>
2022-11-20 23:36:49 -0500

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -5,16 +5,18 @@ do
   
   	touch /tmp/speedtest-result
 	cfspeedtest --json > /tmp/speedtest-result
-  	RESULT=$(cat /tmp/speedtest-result)
 
 	if [[ "${MQTT_PASS}" ]]; then
-	echo "Sending Data to MQTT ($MQTT_SERVER)..."
-		mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC -m $RESULT
+	echo "Sending Data to ($MQTT_SERVER)..."
+		mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -h $MQTT_SERVER -t $MQTT_TOPIC -f /tmp/speedtest-result
 	else
-	echo "Sending Data to MQTT ($MQTT_SERVER) -  No Auth"
-		mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC -m $RESULT
+	echo "Sending Data to ($MQTT_SERVER) with no authentication..."
+		mosquitto_pub -h $MQTT_SERVER -t $MQTT_TOPIC -f /tmp/speedtest-result
 	fi 
-
+	
+	echo "Cleaning up for the next run..."
+	rm /tmp/speedtest-result
+	
 	echo "Sleeping for $SLEEP Seconds..."
 	
 	sleep $SLEEP 
